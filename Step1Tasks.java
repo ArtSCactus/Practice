5664bebb6189451aec06630909ab649ed84b7c50
ArtSCactus <artemir14@gmail.com>
2019-05-27 16:03:03 +0300

    Task 1 done

---
+++ Step1Tasks.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package step1tasks;
+
+public class Step1Tasks {
+public static int task1_1(int a, int b, int c){
+    int z = ((a-3)*b/2)+c;
+    return z;
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("--------Line programs--------");
+        System.out.println("Task 1: " + task1_1(2,4,6));
+    }
+    
+}
