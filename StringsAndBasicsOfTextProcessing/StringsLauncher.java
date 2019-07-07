79831d944efb66cc5c38a9128df5dc531c362955
Artsiom (ArtSCactus) <artemir14@gmail.com>
2019-07-07 23:29:35 +0300

    Update StringsLauncher.java

--- StringsAndBasicsOfTextProcessing/StringsLauncher.java
+++ StringsAndBasicsOfTextProcessing/StringsLauncher.java
@@ -5,57 +5,6 @@
  */
 package Practice.StringsAndBasicsOfTextProcessing;
 
-import Practice.Algorithmization.Decomposition_01;
-import Practice.Algorithmization.Decomposition_02;
-import Practice.Algorithmization.Decomposition_03;
-import Practice.Algorithmization.Decomposition_04;
-import Practice.Algorithmization.Decomposition_05;
-import Practice.Algorithmization.Decomposition_06;
-import Practice.Algorithmization.Decomposition_07;
-import Practice.Algorithmization.Decomposition_08;
-import Practice.Algorithmization.Decomposition_09;
-import Practice.Algorithmization.Decomposition_10;
-import Practice.Algorithmization.Decomposition_11;
-import Practice.Algorithmization.Decomposition_12;
-import Practice.Algorithmization.Decomposition_13;
-import Practice.Algorithmization.Decomposition_14;
-import Practice.Algorithmization.Decomposition_15;
-import Practice.Algorithmization.Decomposition_16;
-import Practice.Algorithmization.Decomposition_17;
-import Practice.Algorithmization.MassiveOfMassives_01;
-import Practice.Algorithmization.MassiveOfMassives_02;
-import Practice.Algorithmization.MassiveOfMassives_03;
-import Practice.Algorithmization.MassiveOfMassives_04;
-import Practice.Algorithmization.MassiveOfMassives_05;
-import Practice.Algorithmization.MassiveOfMassives_06;
-import Practice.Algorithmization.MassiveOfMassives_07;
-import Practice.Algorithmization.MassiveOfMassives_08;
-import Practice.Algorithmization.MassiveOfMassives_09;
-import Practice.Algorithmization.MassiveOfMassives_10;
-import Practice.Algorithmization.MassiveOfMassives_11;
-import Practice.Algorithmization.MassiveOfMassives_12;
-import Practice.Algorithmization.MassiveOfMassives_13;
-import Practice.Algorithmization.MassiveOfMassives_14;
-import Practice.Algorithmization.MassiveOfMassives_15;
-import Practice.Algorithmization.MassiveOfMassives_16;
-import Practice.Algorithmization.OneDimSorting_01;
-import Practice.Algorithmization.OneDimSorting_02;
-import Practice.Algorithmization.OneDimSorting_03;
-import Practice.Algorithmization.OneDimSorting_04;
-import Practice.Algorithmization.OneDimSorting_05;
-import Practice.Algorithmization.OneDimSorting_06;
-import Practice.Algorithmization.OneDimSorting_07;
-import Practice.Algorithmization.OneDimSorting_08;
-import Practice.Algorithmization.OneDim_01;
-import Practice.Algorithmization.OneDim_02;
-import Practice.Algorithmization.OneDim_03;
-import Practice.Algorithmization.OneDim_04;
-import Practice.Algorithmization.OneDim_05;
-import Practice.Algorithmization.OneDim_06;
-import Practice.Algorithmization.OneDim_07;
-import Practice.Algorithmization.OneDim_08;
-import Practice.Algorithmization.OneDim_09;
-import Practice.Algorithmization.OneDim_10;
 import java.util.Scanner;
 
 /**
